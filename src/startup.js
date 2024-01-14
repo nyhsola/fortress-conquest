8a3b1bbfc7f5b193e86bb06775f721f9b7b93806
Nyhsola <nyhsola@gmail.com>
2024-01-14 22:34:20 +0200

    base spawn

--- src/startup.js
+++ src/startup.js
@@ -1,13 +1,17 @@
-import { Rectangle, Region, Unit } from "w3ts"
+import { Rectangle, Unit } from "w3ts"
 import { Players } from "w3ts/globals"
 
 import { Config, UNIT } from "main/Config.js"
+
+const PLAYER_NUMBER = 6
 
 export function initializeGame() {
   print("initializing...")
 
   let config = new Config()
-  let area = Rectangle.fromHandle(config.zone[0])
 
-  Unit.create(Players[0], UNIT.WORKER, area.centerX, area.centerY)
+  for (let i = 0; i < PLAYER_NUMBER; i++) {
+    let area = Rectangle.fromHandle(config.zone[i])
+    let unit = Unit.create(Players[i], UNIT.WORKER, area.centerX, area.centerY)
+  }
 }
